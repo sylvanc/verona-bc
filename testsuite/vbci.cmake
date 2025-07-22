3a1e65e15e7ddead3d8e41a799dd384599f787be
Matthew Parkinson <mattpark@microsoft.com>
2025-07-22 13:22:13 +0100

    Added test harness for building and running vbc programs

---
+++ testsuite/vbci.cmake
@@ -0,0 +1,18 @@
+# Arguments for testing vbci samples
+macro(toolinvoke ARGS testfile outputdir)
+  set(${ARGS} ${testfile}) 
+endmacro()
+
+# Regular expression to match test files
+# This regex matches files with the .infix extension
+set(TESTSUITE_REGEX ".*\\.vbc")
+
+set(TESTSUITE_EXE "$<TARGET_FILE:vbci>")
+
+function (test_output_dir out test)
+  # Use get_filename_component to remove the file extension and keep the directory structure
+  get_filename_component(test_dir ${test} DIRECTORY)
+  get_filename_component(parent_dir ${test_dir} DIRECTORY)
+  # Create the output directory relative to the test directory
+  set(${out} "${parent_dir}/run" PARENT_SCOPE)
+endfunction()
