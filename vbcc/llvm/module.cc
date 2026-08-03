1ec725d630e2655a35192fca2d4e1ce4f1d0afe9
Qianhui Wang <qw304@cam.ac.uk>
2026-08-03 02:52:28 +0100

    configure module construction for LLVM's native/default target triple and data layout.

--- vbcc/llvm/module.cc
+++ vbcc/llvm/module.cc
@@ -1,8 +1,15 @@
 #include "codegen.h"
 
 #include <llvm/IR/Verifier.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Support/FileSystem.h>
+#include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/TargetParser/Host.h>
+#include <memory>
+#include <optional>
 #include <system_error>
 
 namespace vbcc
